935efd37af5c02b7f97a25e37a80d461c15c5f1c
TJ DeVries <devries.timothyj@gmail.com>
2021-08-25 10:16:29 -0400

    feat: Make setup easier

--- README.md
+++ README.md
@@ -14,20 +14,7 @@ require'cmp'.setup {
 
 -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities.textDocument.completion.completionItem.preselectSupport = true
-capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
-capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
-capabilities.textDocument.completion.completionItem.deprecatedSupport = true
-capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
-capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
-capabilities.textDocument.completion.completionItem.resolveSupport = {
-  properties = {
-    'documentation',
-    'detail',
-    'additionalTextEdits',
-  }
-}
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 -- The following example advertise capabilities to `clangd`.
 require'lspconfig'.clangd.setup {
--- lua/cmp_nvim_lsp/init.lua
+++ lua/cmp_nvim_lsp/init.lua
@@ -16,6 +16,34 @@ M.setup = function()
   ]])
 end
 
+local if_nil = function(val, default)
+  if val == nil then return default end
+  return val
+end
+
+M.update_capabilities = function(capabilities, override)
+  override = override or {}
+
+  local completionItem = capabilities.textDocument.completion.completionItem
+
+  completionItem.snippetSupport = if_nil(override.snippetSupport, true)
+  completionItem.preselectSupport = if_nil(override.preselectSupport, true)
+  completionItem.insertReplaceSupport = if_nil(override.insertReplaceSupport, true)
+  completionItem.labelDetailsSupport = if_nil(override.labelDetailsSupport, true)
+  completionItem.deprecatedSupport = if_nil(override.deprecatedSupport, true)
+  completionItem.commitCharactersSupport = if_nil(override.commitCharactersSupport, true)
+  completionItem.tagSupport = if_nil(override.tagSupport, { valueSet = { 1 } })
+  completionItem.resolveSupport = if_nil(override.resolveSupport, {
+    properties = {
+      'documentation',
+      'detail',
+      'additionalTextEdits',
+    }
+  })
+
+  return capabilities
+end
+
 ---Refresh sources on InsertEnter.
 M._on_insert_enter = function()
   local allowed_clients = {}
